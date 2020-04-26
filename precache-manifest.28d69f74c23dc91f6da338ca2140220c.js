657ad8acab5ccd544fa9313ae55ba1d4878edeaa
System Administrator <root@Dimejis-MacBook-Pro.local>
2020-04-26 14:36:18 +0100

    Updates

---
+++ precache-manifest.28d69f74c23dc91f6da338ca2140220c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b71fd7f2048f73fc9ad5d297581419a",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e7f637d0b91f45b28895",
+    "url": "/Robofriends/static/css/main.33bca556.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/Robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "e7f637d0b91f45b28895",
+    "url": "/Robofriends/static/js/main.e76db3da.chunk.js"
+  },
+  {
+    "revision": "a3eff8e1727534c00f37",
+    "url": "/Robofriends/static/js/runtime-main.11a3c7d6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
